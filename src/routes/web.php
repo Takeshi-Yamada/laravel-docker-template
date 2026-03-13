b4162af952466d8f78b96809ca8bbf6df12884a9
takeshi <takeshi.yamada1010@gmail.com>
2026-03-13 16:42:04 +0900

    refactor: 名前付きルートとBladeレイアウト継承によるview構造を整理

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create');
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');
