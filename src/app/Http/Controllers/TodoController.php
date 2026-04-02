b365760686609854b7fa8332315cb77ec67e9d0e
takeshi <takeshi.yamada1010@gmail.com>
2026-04-02 17:48:43 +0900

    refactor: TodoController内のtodoインスタンスの共通化

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,11 +7,18 @@ use App\Todo;
 
 class TodoController extends Controller
 {
-    Public function index()
+    private $todo;
+    #TodoControllerが呼びだされるとコンストラクタが自動で走る。
+    #引数はTodoクラスの$todoを作成
+    public function __construct(Todo $todo)
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        #この関数内で使えるTodoクラスのインスタンスをthis->todoに作成
+        $this->todo =$todo;
+    }
 
+    public function index()
+    {
+        $todos = $this->todo->all();
         return view('todo.index', ['todos' => $todos]);
     }
 
@@ -24,17 +31,14 @@ class TodoController extends Controller
     {
         $inputs = $request->all();
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();
+        $this->todo->fill($inputs);
+        $this->todo->save();
         return redirect()->route('todo.index');
     }
 
     Public function show($id)
     {
-        $model = new Todo;
-        $todo = $model->find($id);
-
+        $todo = $this->todo->find($id);
         return view('todo.show', ['todo' => $todo]);
     }
 }
